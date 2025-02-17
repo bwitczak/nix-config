b6e0110e77b0e3cf144c36646656e99ca367bd65
Błażej Witczak <blazej.witczak@nsure.com>
2025-02-17 19:15:42 +0100

    Update and replace Nvim with vi

--- darwin/default.nix
+++ darwin/default.nix
@@ -7,10 +7,16 @@
 #       ├─ darwin-configuraiton.nix
 #       └─ <host>.nix
 #
-
-{ inputs, nixpkgs, nixpkgs-stable, darwin, home-manager, nixvim, vars, ... }:
-
-let
+{
+  inputs,
+  nixpkgs,
+  nixpkgs-stable,
+  darwin,
+  home-manager,
+  nixvim,
+  vars,
+  ...
+}: let
   systemConfig = system: {
     system = system;
     pkgs = import nixpkgs {
@@ -22,21 +28,20 @@ let
       config.allowUnfree = true;
     };
   };
-in
-{
-  m1 =
-    let
-      inherit (systemConfig "aarch64-darwin") system pkgs stable;
-    in
+in {
+  m1 = let
+    inherit (systemConfig "aarch64-darwin") system pkgs stable;
+  in
     darwin.lib.darwinSystem {
       inherit system;
-      specialArgs = { inherit inputs system pkgs stable;
-	vars = {
-	  user = "bwitczak";
-        location = "$HOME/.setup";
-        terminal = "alacritty";
-        editor = "nvim";
-	};
+      specialArgs = {
+        inherit inputs system pkgs stable;
+        vars = {
+          user = "bwitczak";
+          location = "$HOME/.setup";
+          terminal = "alacritty";
+          editor = "vi";
+        };
       };
       modules = [
         ./work.nix
