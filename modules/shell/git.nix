26a0b08897946fb068165561d49937fcc110b1e6
Błażej Witczak <blazej.witczak@nsure.com>
2025-05-21 23:23:50 +0200

    Fix #3

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -6,31 +6,31 @@
     enable = true;
     userName = "bwitczak";
     userEmail = "blaise.witczak@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-      color.ui = "auto";
-      diff = {
-        submodule = "log";
-        tool = "meld";
-      };
-      difftool.promt = false;
-      commit.verbose = true;
-      rerere = {
-        enabled = 1;
-        autoupdate = true;
-      };
-      status.submoduleSummary = -1;
-      submodule.fetchJobs = 0;
-      rebase.missingCommitsCheck = "warn";
-      merge.tool = "meld";
-      url = {
-        "ssh://git@gitlab.com/" = {
-          insteadOf = "https://gitlab.com/";
-        };
-      };
-      credential.helper = "store";
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-    };
+    # extraConfig = {
+    #   init.defaultBranch = "main";
+    #   color.ui = "auto";
+    #   diff = {
+    #     submodule = "log";
+    #     tool = "meld";
+    #   };
+    #   difftool.promt = false;
+    #   commit.verbose = true;
+    #   rerere = {
+    #     enabled = 1;
+    #     autoupdate = true;
+    #   };
+    #   status.submoduleSummary = -1;
+    #   submodule.fetchJobs = 0;
+    #   rebase.missingCommitsCheck = "warn";
+    #   merge.tool = "meld";
+    #   url = {
+    #     "ssh://git@gitlab.com/" = {
+    #       insteadOf = "https://gitlab.com/";
+    #     };
+    #   };
+    #   credential.helper = "store";
+    #   pull.rebase = true;
+    #   push.autoSetupRemote = true;
+    # };
   };
 }
