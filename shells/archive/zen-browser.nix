88a64bf730d05ddaa74730b7f4745fed533ebde3
bwitczak <blaise.witczak@gmail.com>
2025-09-14 12:07:15 +0200

    Update Zen browser

--- shells/archive/zen-browser.nix
+++ shells/archive/zen-browser.nix
@@ -1,40 +1,37 @@
 # Zen Browser AppImage installer for Nix
 # This installs Zen Browser from the official AppImage
 # Run with: nix build -f zen-browser.nix
-
-{ pkgs ? import <nixpkgs> { } }:
-
-let
+{pkgs ? import <nixpkgs> {}}: let
   pname = "zen-browser";
-  version = "1.12.7b";
+  version = "1.15.5b";
 in
-pkgs.appimageTools.wrapType2 {
-  inherit pname version;
+  pkgs.appimageTools.wrapType2 {
+    inherit pname version;
 
-  src = pkgs.fetchurl {
-    # url = "https://updates.zen-browser.app/releases/zen-browser-${version}-x86_64.AppImage";
-    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
-    sha256 = "70fdc51db5ac00a4d1717d32b0aebc0ffad20f5ea6944ff204378dabacbc8291";
-  };
+    src = pkgs.fetchurl {
+      # url = "https://updates.zen-browser.app/releases/zen-browser-${version}-x86_64.AppImage";
+      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-x86_64.AppImage";
+      sha256 = "b4742133114b6e43195477db341820e4436a0c7382e9921c4abbb8af5df66488";
+    };
 
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications
-    cat > $out/share/applications/zen-browser.desktop <<EOF
-    [Desktop Entry]
-    Type=Application
-    Name=Zen Browser
-    Comment=A calmer internet browser
-    Exec=$out/bin/zen-browser
-    Icon=zen-browser
-    Categories=Network;WebBrowser;
-    EOF
-  '';
+    extraInstallCommands = ''
+      mkdir -p $out/share/applications
+      cat > $out/share/applications/zen-browser.desktop <<EOF
+      [Desktop Entry]
+      Type=Application
+      Name=Zen Browser
+      Comment=A calmer internet browser
+      Exec=$out/bin/zen-browser
+      Icon=zen-browser
+      Categories=Network;WebBrowser;
+      EOF
+    '';
 
-  meta = with pkgs.lib; {
-    description = "A calmer internet browser";
-    homepage = "https://zen-browser.app";
-    license = licenses.mpl20;
-    maintainers = [];
-    platforms = [ "x86_64-linux" ];
-  };
-} 
+    meta = with pkgs.lib; {
+      description = "A calmer internet browser";
+      homepage = "https://zen-browser.app";
+      license = licenses.mpl20;
+      maintainers = [];
+      platforms = ["x86_64-linux"];
+    };
+  }
