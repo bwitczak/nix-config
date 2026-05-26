b17fb54291ab0940e26f33d7d42210566af06922
bwitczak <blaise.witczak@gmail.com>
2026-05-26 08:15:14 +0200

    Change Docker to Podman

--- modules/desktops/virtualisation/docker.nix
+++ modules/desktops/virtualisation/docker.nix
@@ -1,19 +1,29 @@
 #
 #  Docker
 #
-{
-  pkgs,
-  vars,
-  ...
-}: {
+{pkgs, ...}: {
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+
   virtualisation = {
-    docker.enable = true;
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for docker compose stacks to resolve service names on the default network.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
+    containers.containersConf.settings.engine = {
+      compose_providers = ["${pkgs.docker-compose}/bin/docker-compose"];
+      # compose_warning_logs = false;
+    };
   };
 
-  users.groups.docker.members = ["${vars.user}"];
-
   environment.systemPackages = with pkgs; [
-    docker # Containers
-    # docker-compose # Multi-Container
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    docker-compose # `docker compose` provider (via podman + dockerCompat)
   ];
 }
